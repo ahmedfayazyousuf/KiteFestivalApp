e99831c65dd3625b4a8ca70c7056d0016983fc72
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2023-01-31 22:56:32 +0400

    Update index.js

--- src/components/Success/index.js
+++ src/components/Success/index.js
@@ -10,10 +10,37 @@ const Success = () =>{
             
             <div style={{display: 'flex', flexDirection: 'column', height: '100%', width: '100%'}}>
                 
-                <div style={{display: 'flex', flexDirection: 'column', height: '60%', width: '100%', backgroundColor: 'yellow'}}>
+                <div style={{display: 'flex', flexDirection: 'column', height: '62%', width: '100%'}}>
                 </div>
 
-                <div style={{display: 'flex', alignItems: 'center', textAlign: 'center', flexDirection: 'column', height: '30%', width: '100%', backgroundColor: 'red'}}>
+                <div style={{display: 'flex', alignItems: 'center', textAlign: 'center', flexDirection: 'column', height: '38%', width: '100%'}}>
+                    <h1 style={{fontSize: '20px', color: 'white', fontWeight: '500'}}>Booking Details</h1>
+
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '400px'}}>
+
+                        <table>
+                            <tr>
+                                <th style={{textAlign: 'left', color: 'white'}}>Name</th>
+                                <th style={{width: '40px', color: 'white'}}>:</th>
+                                <th style={{textAlign: 'left', color: 'white'}}>Ahmed</th>
+                            </tr>
+                            <tr>
+                                <td style={{textAlign: 'left', color: 'white'}}>Location</td>
+                                <td style={{width: '40px', color: 'white'}}>:</td>
+                                <td style={{textAlign: 'left', color: 'white'}}>ACTIVITY AREA 1</td>
+                            </tr>
+                            <tr>
+                                <td style={{textAlign: 'left', color: 'white'}}>Date</td>
+                                <td style={{width: '40px', color: 'white'}}>:</td>
+                                <td style={{textAlign: 'left', color: 'white'}}>9 Feb</td>
+                            </tr>
+                            <tr>
+                                <td style={{textAlign: 'left', color: 'white'}}>Time</td>
+                                <td style={{width: '40px', color: 'white'}}>:</td>
+                                <td style={{textAlign: 'left', color: 'white'}}>6:30 PM</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
            

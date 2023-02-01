31a3842c5bc3c7abee86a511fa97cab721085472
hsggamer <55681300+hsggamer@users.noreply.github.com>
2023-02-01 11:01:11 +0400

    Update index.js

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -76,6 +76,7 @@ const Registration = () =>{
 
         var area = location.state.time.slice(0,7)
         var time = location.state.time.slice(7)
+        var date = location.state.date
 
         Users.add({
             Name:Name,
@@ -83,6 +84,7 @@ const Registration = () =>{
             Number:Number,
             Time:time,
             Area:area,
+            Date: date,
             time: firebase.firestore.FieldValue.serverTimestamp()
 
         }).then( function(docRef) {
